a1aab669b2bed7ba26645053c5ee48877d54c4af
onwuasoanya_george <ndu4george@gmail.com>
2020-10-03 16:52:27 +0100

    node for beginers. testing out importing modules.

---
+++ app.js
@@ -0,0 +1,11 @@
+const sum = (a, b) => a + b;
+const PI = 3.142;
+class SomeTopClass {
+    sayHi() {
+        console.log("Hello");
+    }
+}
+
+module.exports.sum = sum;
+module.exports.PI = PI;
+module.exports.SomeTopClass = SomeTopClass;
---
+++ tuts.js
@@ -0,0 +1,6 @@
+const app = require("./app");
+
+console.log(app);
+console.log(app.PI);
+console.log(app.sum(4, 6));
+console.log(new app.SomeTopClass());
