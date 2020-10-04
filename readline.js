03e3626fea5cc0a52b2f62e582403c4042c799e2
onwuasoanya_george <ndu4george@gmail.com>
2020-10-04 13:17:08 +0100

    read and write to console with the readline module

---
+++ readline.js
@@ -0,0 +1,20 @@
+const readLine = require('readline');
+const rLI = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+let num1 = Math.floor(Math.random() * 10 + 1);
+let num2 = Math.floor(Math.random() * 10 + 1);
+let ans = num1 + num2;
+
+rLI.question(`What is the sum of ${num1} and ${num2}? \n`, (answer) => {
+    if (ans == answer.trim()) {
+        console.log('\nCorrect!');
+        rLI.close();
+    } else {
+        console.log(`\nWrong! The correct answer is ${ans}.`)
+        rLI.close();
+    }
+})
